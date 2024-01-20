9c9932d47ddf9a2fa39ccf27f734eafadb1a78b6
student <>
2024-01-20 10:19:47 +0700

    ahaha

--- main.py
+++ main.py
@@ -1,11 +1,2 @@
-class Student:
- def __init__(self, name, age, group, evl):
-    self.name = name
-    self.age = age
-    self.group = group
-    self.evl = evl
- def cal_evl(self):
-     return sum(self.evl)/len(self.evl) if len(self.evl) !=0 else 0
-tom= Student("Tom",2,"IS(PRO)-11",[3,4,5,4,5,3,4,5,5])
-evl_z=tom.cal_evl()
-print(f"Средняя оценка студента{tom.name} равна {evl_z}")
+hah=10
+print(hah)
